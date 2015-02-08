b4aac068c8d4730c19b7d4633a2b2a82dea4856d
Sai-Sun <sunshine881112@hotmail.com>
2015-02-08 17:52:18 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+library(sqldf)
+
+data<-read.csv.sql(file.choose(),header=T,sep=";",
+		sql="select*from file where(Date=='1/2/2007' OR Date=='2/2/2007')")
+
+names(data)
+
+temp<-with(data,paste(Date,Time))
+datetime<-strptime(temp,"%d/%m/%Y %H:%M:%S")
+
+new.data<-data.frame(datetime,data[,-c(1,2)])
+
+with(new.data,hist(Global_active_power,col="red",
+	xlab="Global Active Power (kilowatts)",
+	main="Global Active Power"))
+
+dev.copy(png,file="plot1.png",width=480,height=480)
+dev.off()
